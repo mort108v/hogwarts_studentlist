c84ce27b3ecb24b954c9d961dcd2efeaeec6294a
Morten Six <mort108v@stud.kea.dk>
2021-09-20 20:27:21 +0200

    Attempt to export JSON students from studentlist.js to hogwarts.js and appending to table in DOM

--- js/hogwarts.js
+++ js/hogwarts.js
@@ -3,7 +3,34 @@
 // Import students
 import * as studentlist from './studentlist.js'
 
-
-
+addEventListener("DOMContentLoaded", displayList(studentlist.allStudents))
 
 // Display studentlist
+function displayList(students) {
+    // clear the list
+    document.querySelector("#list tbody").innerHTML = ""
+
+    // build a new list
+    students.forEach(displayStudent)
+}
+
+function displayStudent(student) {
+
+    // create clone
+    const clone = document.querySelector("template#student").content.cloneNode(true)
+
+    // set clone data
+    clone.querySelector("[data-field=firstname]").textContent = student.firstname
+    clone.querySelector("[data-field=middlename]").textContent = student.middlename
+    clone.querySelector("[data-field=lastname]").textContent = student.lastname
+    clone.querySelector("[data-field=nickname]").textContent = student.nickname
+    clone.querySelector("[data-field=gender]").textContent = student.gender
+    clone.querySelector("[data-field=bloodstatus]").textContent = student.bloodstatus
+    clone.querySelector("[data-field=house]").textContent = student.house
+    clone.querySelector(".student-photo").src = student.image
+
+    // append clone to list
+    document.querySelector("#list tbody").appendChild(clone)
+}
+
+console.table(studentlist)
--- js/studentlist.js
+++ js/studentlist.js
@@ -1,5 +1,7 @@
 "use strict"
 
+
+
 const Student = {
     firstname: " ",
     middlename: " ",
@@ -13,12 +15,85 @@ const Student = {
     isExpelled: false,
 }
 
+let allStudentVariables = {
+    allStudents = [],
+    filter,
+    filteredStudents = [],
+    sortedStudents = [],
+    prefects = [],
+    inquisitorialSquad = []
+}
+
+loadJSON
+
 // async function loadJSON()
+async function loadJSON() {
+    const response = await fetch("https://petlatkea.dk/2021/hogwarts/students.json")
+    const jsonData = await response.json()
+    prepareObjects(jsonData)
+}
 
 // function prepareObjects(jsonData)
+function prepareObjects(jsonData) {
+    console.log("JSON Loaded")
+    allStudentVariables.allStudents = jsonData.map(makeStudents)
+    allStudentVariables.filteredStudents = allStudentVariables.allStudents
+        // displayList(allStudents)
+}
 
 // function makeStudents(jsonObject)
 // 	firstName | middleName | lastName | nickname | gender | house | isPureblood | isHalfblood | isPlainMuggle | isPrefect | isInquis
+function makeStudents(jsonObject) {
+    console.log("Student in the making")
+
+    const student = Object.create(Student)
+        // Student Full Name 
+    const studentFullName = jsonObject.fullname.substring(0).toLowerCase().trim()
+    const studentFullNameSplit = studentFullName.split(" ")
+
+    // Student First Name
+    const studentFirstname = studentFullNameSplit[0]
+    student.firstname = studentFirstname.charAt(0).toUpperCase() + studentFirstname.substring(1)
+
+    // Student Middle Name
+    if (studentFullNameSplit.length > 2) {
+        const studentMiddlename = studentFullNameSplit[1]
+        student.middlename = studentMiddlename.charAt(0).toUpperCase() + studentMiddlename.substring(1)
+    }
+
+    if (studentFullNameSplit.length > 2) {
+        // Student Last Name
+        const studentLastname = studentFullNameSplit[2]
+        student.lastname = studentLastname.charAt(0).toUpperCase() + studentLastname.substring(1)
+    } else if (studentFullNameSplit.length == 2) {
+        const studentLastnameTwo = studentFullNameSplit[1]
+        student.lastname = studentLastnameTwo.charAt(0).toUpperCase() + studentLastnameTwo.substring(1)
+    }
+
+    // Student Nickname
+    if (studentFullName.includes(`"`)) {
+        student.middlename = ""
+        const studentNickname = studentFullNameSplit[1]
+        student.nickname = `"` + studentNickname.charAt(1).toUpperCase() + studentNickname.substring(2)
+    }
+
+    // Student Gender
+    const studentGender = jsonObject.gender.substring().toLowerCase().trim()
+    student.gender = studentGender.charAt(0).toUpperCase() + studentGender.substring(1)
+
+    // Student House
+    const studentHouse = jsonObject.house.substring().toLowerCase().trim()
+    student.house = studentHouse.charAt(0).toUpperCase() + studentHouse.substring(1)
+
+    // Student Image
+    const studentImageURL = "studentphotos/";
+    student.image = studentImageURL + student.lastname.toLowerCase() + "_" + student.firstname.charAt(0).toLowerCase() + ".png"
+
+    return student
+}
+
+export { allStudentVariables, Student }
+
 
 // Sort students
 // function sortStudents(sort, sortDirection)
