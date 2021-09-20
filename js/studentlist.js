4100c8a3fe5fad2056e8c7a41e542df24f780971
Morten Six <mort108v@stud.kea.dk>
2021-09-21 00:01:43 +0200

    Sorting and filtering working and cleaned up a bit

--- js/studentlist.js
+++ js/studentlist.js
@@ -24,7 +24,11 @@ let allStudentVariables = {
     inquisitorialSquad: []
 }
 
-
+export let allConstants = {
+    filterButtons: document.querySelectorAll(`[data-action="filter"]`),
+    sortButtons: document.querySelectorAll(`[data-action="sort"]`),
+    studentTemplates: document.querySelectorAll(`[data-field=firstname]`),
+}
 
 // async function loadJSON()
 export async function loadJSON() {
@@ -120,15 +124,217 @@ function makeStudents(jsonObject) {
     return student
 }
 
-// export { allStudentVariables }
-
-
 // Sort students
 // function sortStudents(sort, sortDirection)
 // 	firstName | lastName | house | prefects | inquisitorialSquad
+export function listenForBTNclick() {
+    console.log("button listen");
+
+    // Add event-listeners to filter and sort buttons
+    allConstants.sortButtons.forEach((sortButton) => {
+        sortButton.addEventListener("click", clickSortButton);
+    });
+
+    allConstants.filterButtons.forEach((filterButton) => {
+        filterButton.addEventListener("click", clickFilterButton);
+    });
+}
+
+function clickSortButton(sortButton) {
+    console.log("Sorting Clicked");
+
+    const sort = sortButton.target.dataset.sort;
+    const sortDirection = sortButton.target.dataset.sortDirection;
+
+    console.log("Dataset", sortButton.target.dataset);
+
+    const sortedStudents = sortStudents(sort, sortDirection);
+    sortButton.target.dataset.sortDirection = sortDirection === "asc" ? "desc" : "asc";
+    console.log("My Sorted Students ", sortedStudents);
+
+    displayList(sortedStudents);
+}
+
+function sortStudents(sort, sortDirection, ) {
+    console.log("Sorting the student list", sort, allStudentVariables.filteredStudents);
+
+    switch (sort) {
+        case "firstname":
+            allStudentVariables.sortedStudents = allStudentVariables.filteredStudents.sort((a, b) => {
+                if (sortDirection === "asc") {
+                    // Ascending
+                    return a.firstname > b.firstname ? 1 : -1;
+                } else {
+                    // Implicit descending
+                    return a.firstname > b.firstname ? -1 : 1;
+                }
+            });
+            break;
+        case "middlename":
+            allStudentVariables.sortedStudents = allStudentVariables.filteredStudents.sort((a, b) => {
+                if (sortDirection === "asc") {
+                    // Ascending
+                    return a.middlename > b.middlename ? 1 : -1;
+                } else {
+                    // Implicit descending
+                    return a.middlename > b.middlename ? -1 : 1;
+                }
+            });
+            break;
+        case "lastname":
+            allStudentVariables.sortedStudents = allStudentVariables.filteredStudents.sort((a, b) => {
+                if (sortDirection === "asc") {
+                    // Ascending
+                    return a.lastname > b.lastname ? 1 : -1;
+                } else {
+                    // Implicit descending
+                    return a.lastname > b.lastname ? -1 : 1;
+                }
+            });
+            break;
+        case "nickname":
+            allStudentVariables.sortedStudents = allStudentVariables.filteredStudents.sort((a, b) => {
+                if (sortDirection === "asc") {
+                    // Ascending
+                    return a.nickname > b.nickname ? 1 : -1;
+                } else {
+                    // Implicit descending
+                    return a.nickname > b.nickname ? -1 : 1;
+                }
+            });
+            break;
+        case "housesort":
+            allStudentVariables.sortedStudents = allStudentVariables.filteredStudents.sort((a, b) => {
+                if (sortDirection === "asc") {
+                    // Ascending
+                    return a.house > b.house ? 1 : -1;
+                } else {
+                    // Implicit descending
+                    return a.house > b.house ? -1 : 1;
+                }
+            });
+            break;
+        case "inquis":
+            allStudentVariables.sortedStudents = allStudentVariables.filteredStudents.sort((a, b) => {
+                if (sortDirection === "asc") {
+                    // Ascending
+                    return a.inquis > b.inquis ? 1 : -1;
+                } else {
+                    // Implicit descending
+                    return a.inquis > b.inquis ? -1 : 1;
+                }
+            });
+            break;
+        case "prefect":
+            allStudentVariables.sortedStudents = allStudentVariables.filteredStudents.sort((a, b) => {
+                if (sortDirection === "asc") {
+                    // Ascending
+                    return a.prefect > b.prefect ? 1 : -1;
+                } else {
+                    // Implicit descending
+                    return a.prefect > b.prefect ? -1 : 1;
+                }
+            });
+            break;
+        case "gender":
+            allStudentVariables.sortedStudents = allStudentVariables.filteredStudents.sort((a, b) => {
+                if (sortDirection === "asc") {
+                    // Ascending
+                    return a.prefect > b.prefect ? 1 : -1;
+                } else {
+                    // Implicit descending
+                    return a.prefect > b.prefect ? -1 : 1;
+                }
+            });
+            break;
+
+        default:
+            console.error("Unsupported Sorter", sort);
+    }
+    return allStudentVariables.sortedStudents;
+}
+
 // Filter students
 // function filterStudents()
 // 	house | responsibilities | expelled | nonExpelled
+
+function clickFilterButton(filterButton) {
+    console.log("filterClicked");
+
+    let filter
+    filter = filterButton.target.dataset.filter;
+
+    const filteredStudents = filterStudents(filter);
+    console.log(filteredStudents);
+    displayList(filteredStudents);
+}
+
+function filterStudents(filter) {
+    console.log("Filtering My Students");
+
+
+    let filtered = allStudentVariables.filteredStudents
+    let allStudents = allStudentVariables.allStudents
+
+    filtered = [];
+
+    if (filter === 'prefect') {
+        filtered = allStudents.filter(isPrefect)
+    } else if (filter === "inquis") {
+        filtered = allStudents.filter(isInquis)
+    } else if (filter === "slytherin") {
+        filtered = allStudents.filter(isSlytherin)
+    } else if (filter === "ravenclaw") {
+        filtered = allStudents.filter(isRavenclaw)
+    } else if (filter === "hufflepuff") {
+        filtered = allStudents.filter(isHufflepuff)
+    } else if (filter === "gryffindor") {
+        filtered = allStudents.filter(isGryffindor)
+    } else {
+        filtered = allStudents.filter(isAll)
+    }
+
+    return filtered
+}
+
+// Students afilliation functions
+// function checkStudentAfilliation(student) {
+//     if (student.house === "Slytherin") return true
+//     if (student.house === "Hufflepuff") return true
+//     if (student.house === "Gryffindor") return true
+//     if (student.house === "Ravenclaw") return true
+//     if (student.prefect) return true
+//     if (student.inquis) return true
+//     else if (student) return true
+// }
+
+function isSlytherin(student) {
+    if (student.house === "Slytherin") return true
+}
+
+function isHufflepuff(student) {
+    if (student.house === "Hufflepuff") return true
+}
+
+function isGryffindor(student) {
+    if (student.house === "Gryffindor") return true
+}
+
+function isRavenclaw(student) {
+    if (student.house === "Ravenclaw") return true
+}
+
+function isPrefect(student) {
+    if (student.prefect) return true
+}
+
+function isInquis(student) {
+    if (student.inquis) return true
+}
+
+function isAll(student) {
+    if (student) return true
+}
 // About info
 // function studentNumberInfo()
 // 	numberOfStudentsInHouse | totalNumberOfStudents | numberOfStudentsExpelled | numberOfStudentsDisplayed
